2433a9ba3c5429e766967643e19cb6b123b485c5
k.piranishvili <k.piranishvili@crystalbet.com>
2024-06-13 11:11:48 +0400

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,12 +7,5 @@ export default defineConfig({
   // server: {
   //   host: "0.0.0.0",
   // },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "nested/index.html"),
-      },
-    },
-  },
+  build: { chunkSizeWarningLimit: 1600 },
 });
