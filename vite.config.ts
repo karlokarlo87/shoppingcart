d76001b67d640d10033173ea6c8c3988f30c44de
k.piranishvili <k.piranishvili@crystalbet.com>
2024-06-13 11:06:26 +0400

    add

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-  },
+  // server: {
+  //   host: "0.0.0.0",
+  // },
+  build: { chunkSizeWarningLimit: 1600 },
 });
